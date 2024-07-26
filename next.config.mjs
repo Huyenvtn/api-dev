15bb828dd1084130c0875fc194e744718c93ec03
huyenvtn <huyenvtn0210@gmail.com>
2024-07-26 17:31:35 +0700

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+// ----------------------------------------------------------------------
+
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    DEV_API: 'http://localhost:7272',
+    PRODUCTION_API: 'https://minimal-assets-api.vercel.app',
+  },
+};
+
+export default nextConfig;
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import { HOST_API } from '../config';
+// ----------------------------------------------------------------------
+
+export default function IndexPage() {
+  console.log('HOST_API', HOST_API);
+
+  return <h1>The starting point for your next project</h1>;
+}
