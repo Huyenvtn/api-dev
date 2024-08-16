eb83c3fd52d1e2bd9971d19717bf2bc890135dbe
huyenvtn <huyenvtn0210@gmail.com>
2024-08-16 17:19:19 +0700

    mock request api

--- src/_mock/_event.ts
+++ src/_mock/_event.ts
@@ -9,97 +9,103 @@ import COLORS from '../colors.json';
 
 // ----------------------------------------------------------------------
 
-const primaryMain = COLORS.primary.main;
-const secondaryMain = COLORS.secondary.main;
-const infoMain = COLORS.info.main;
-const infoDarker = COLORS.info.darker;
-const successMain = COLORS.success.main;
-const warningMain = COLORS.warning.main;
-const errorMain = COLORS.error.main;
-const errorDarker = COLORS.error.darker;
+const accepted = COLORS.success.main;
+const planned = COLORS.grey[500];
+const rejected = COLORS.error.main;
+const requested = COLORS.warning.main;
 
 const _events = [
   {
     id: _mock.id(1),
-    title: _mock.eventNames(1),
-    allDay: false,
-    color: primaryMain,
+    title: 'annual leave',// ca sáng
+    allDay: true,
+    color: accepted,
     description: _mock.description(1),
-    start: fSub({ days: 12, hours: 3, minutes: 30 }),
-    end: fSub({ days: 12, hours: 0, minutes: 0 }),
+    start: fSub({ days: 17, hours: 13, minutes: 0 }),
+    end: fSub({ days: 13, hours: 18, minutes: 0 }),
   },
   {
-    id: _mock.id(2),
-    title: _mock.eventNames(2),
-    allDay: false,
-    color: infoMain,
-    description: _mock.description(2),
-    start: fSub({ days: 6, hours: 3, minutes: 30 }),
-    end: fSub({ days: 6, hours: 0, minutes: 0 }),
+    id: _mock.id(1),
+    title: 'annual leave', // ca trưa
+    allDay: true,
+    color: accepted,
+    description: _mock.description(1),
+    start: fSub({ days: 16, hours: 13, minutes: 0 }),
+    end: fSub({ days: 13, hours: 18, minutes: 0 }),
   },
   {
-    id: _mock.id(3),
-    title: _mock.eventNames(3),
-    allDay: false,
-    color: successMain,
-    description: _mock.description(3),
-    start: fAdd({ days: 3, hours: 0, minutes: 0 }),
-    end: fAdd({ days: 3, hours: 4, minutes: 15 }),
+    id: _mock.id(1),
+    title: 'annual leave', // ca chiều
+    allDay: true,
+    color: accepted,
+    backgroundColor: '#ff0000',
+    description: _mock.description(1),
+    start: fSub({ days: 16, hours: 13, minutes: 0 }),
+    end: fSub({ days: 14, hours: 18, minutes: 0 }),
   },
   {
-    id: _mock.id(4),
-    title: _mock.eventNames(4),
-    allDay: false,
-    color: secondaryMain,
-    description: _mock.description(4),
-    start: fSub({ days: 0, hours: 4, minutes: 0 }),
-    end: fSub({ days: 0, hours: 0, minutes: 0 }),
+    id: _mock.id(1),
+    title: 'annual leave',// ca sáng
+    allDay: true,
+    color: requested,
+    description: _mock.description(1),
+    start: fSub({ days: 6, hours: 13, minutes: 0 }),
+    end: fSub({ days: 3, hours: 18, minutes: 0 }),
+  },
+  {
+    id: _mock.id(1),
+    title: 'annual leave', // ca trưa
+    allDay: true,
+    color: requested,
+    description: _mock.description(1),
+    start: fSub({ days: 5, hours: 13, minutes: 0 }),
+    end: fSub({ days: 3, hours: 18, minutes: 0 }),
+  },
+  {
+    id: _mock.id(1),
+    title: 'annual leave', // ca chiều
+    allDay: true,
+    color: requested,
+    description: _mock.description(1),
+    start: fSub({ days: 4, hours: 13, minutes: 0 }),
+    end: fSub({ days: 3, hours: 18, minutes: 0 }),
   },
   {
     id: _mock.id(5),
-    title: _mock.eventNames(5),
-    allDay: false,
-    color: warningMain,
+    title: 'sick leave',
+    allDay: true,
+    color: requested,
     description: _mock.description(5),
-    start: fAdd({ days: 3, hours: 0, minutes: 0 }),
-    end: fAdd({ days: 3, hours: 0, minutes: 30 }),
+    start: fAdd({ days: 1, hours: 0, minutes: 0 }),
+    end: fAdd({ days: 1, hours: 0, minutes: 30 }),
   },
   {
     id: _mock.id(6),
-    title: _mock.eventNames(6),
+    title: 'married leave',
     allDay: true,
-    color: errorMain,
+    color: rejected,
     description: _mock.description(6),
-    start: dayjs(fSub({ days: 3 }))
+    start: dayjs(fSub({ days: 2 }))
       .startOf('day')
       .format(),
-    end: dayjs(fSub({ days: 3 }))
+    end: dayjs(fSub({ days: 1 }))
       .endOf('day')
       .format(),
   },
   {
     id: _mock.id(7),
-    title: _mock.eventNames(7),
-    allDay: false,
-    color: infoDarker,
+    title:'annual leave',
+    allDay: true,
+    color: accepted,
     description: _mock.description(7),
-    start: fAdd({ days: 3, hours: 0, minutes: 15 }),
-    end: fAdd({ days: 3, hours: 0, minutes: 30 }),
-  },
-  {
-    id: _mock.id(8),
-    title: _mock.eventNames(8),
-    allDay: false,
-    color: infoMain,
-    description: _mock.description(8),
-    start: fAdd({ days: 3, hours: 0, minutes: 45 }),
-    end: fAdd({ days: 3, hours: 0, minutes: 55 }),
+    start: fAdd({ days: 0, hours: 0, minutes: 15 }),
+    end: fAdd({ days: 0, hours: 0, minutes: 30 }),
   },
   {
     id: _mock.id(9),
-    title: _mock.eventNames(9),
-    allDay: false,
-    color: errorDarker,
+    title: 'annual leave',
+    allDay: true,
+    color: planned,
     description: _mock.description(9),
     start: fAdd({ days: 6, hours: 0, minutes: 10 }),
     end: fAdd({ days: 6, hours: 0, minutes: 20 }),
--- src/_mock/_request.ts
+++ src/_mock/_request.ts
@@ -26,3 +26,9 @@ export const _requests = [...Array(20)].map((_, index) => {
     updatedAt: _mock.time(index),
   };
 });
+
+export const _summary = [
+  { id: 'annual', name: 'Annual Leave', actual: 1, simulated: 1, entitled: 25, taken: 24, planned: 2, requested: 3},
+  { id: 'sick', name: 'Sick Leave', actual: 1, simulated: 1, entitled: 25, taken: 24, planned: 2, requested: 3},
+  { id: 'married', name: 'Married Leave', actual: 2, simulated: 2, entitled: 3, taken: 1, planned: 2, requested: 1 },
+];
